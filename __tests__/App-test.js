a93caef2faeba44572975d955aca00fcd6015510
Omar Maroki <omar.maroki@tappit.com>
2023-01-18 11:33:52 +0000

    Please feel free to test me on testing! However, setting up test env here may take a while. 🤕

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,13 +5,18 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import HomeScreen from '../src/screens/HomeScreen';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import HomeScreen from '../src/screens/HomeScreen';
+const queryClient = new QueryClient();
+const wrapper = ({ children }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
 
 it('renders correctly', () => {
-  renderer.create(<App />).toJSON();
+  renderer.create(wrapper(<App />)).toJSON();
 });
 
 it('renders the home screen', () => {
--- store.ts
+++ store.ts
@@ -1,25 +1,38 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
 
 interface ScreenState {
   selectedRetailer: string;
   setSelectedRetailer: (retailer: string) => void;
 }
 
-const useRetailerStore = create<ScreenState>()(
-  devtools(
-    persist(
-      set => ({
-        selectedRetailer: '',
-        setSelectedRetailer: retailer => set({ selectedRetailer: retailer }),
-      }),
-      {
-        name: 'retailer-storage',
-        getStorage: () => AsyncStorage,
-      },
-    ),
-  ),
-);
+const useRetailerStore = create<ScreenState>()(set => ({
+  selectedRetailer: '',
+  setSelectedRetailer: retailer => set({ selectedRetailer: retailer }),
+}));
 
 export default useRetailerStore;
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { create } from 'zustand';
+// import { devtools, persist } from 'zustand/middleware';
+
+// interface ScreenState {
+//   selectedRetailer: string;
+//   setSelectedRetailer: (retailer: string) => void;
+// }
+
+// const useRetailerStore = create<ScreenState>()(
+//   devtools(
+//     persist(
+//       set => ({
+//         selectedRetailer: '',
+//         setSelectedRetailer: retailer => set({ selectedRetailer: retailer }),
+//       }),
+//       {
+//         name: 'retailer-storage',
+//         getStorage: () => AsyncStorage,
+//       },
+//     ),
+//   ),
+// );
+
+// export default useRetailerStore;
